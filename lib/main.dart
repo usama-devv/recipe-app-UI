1b329174ba569a7b4b64fb6dcace6841b817519b
Usama Masood <134695768+usama-devv@users.noreply.github.com>
2024-09-12 13:11:20 +0500

    added splash screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:recipe_app_ui/views/home_screen.dart';
+import 'package:recipe_app_ui/views/splash_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -17,7 +18,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const HomeScreen(),
+      home: const SplashScreen(),
     );
   }
 }
