dccb16e90584576d2a873bc39de5df4735a805b2
Usama Masood <134695768+usama-devv@users.noreply.github.com>
2024-09-12 13:28:48 +0500

    added colors file

---
+++ lib/utils/constants/colors.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class UColors {
+  UColors._();
+
+  /// App Colors
+  static const Color primaryColor = Color(0xFFFF9B05);
+  static const Color secondaryColor = Color(0xFF519BE0);
+  static const Color accentColor = Color(0xFFFFCC00);
+  static const Color successColor = Color(0xFF00C337);
+  static const Color whiteColor = Color(0xFFFFFFFF);
+  static const Color silverColor = Color(0xFFBFBFBF);
+  static const Color silverGreyColor = Color(0xFFC1C1C1);
+  static const Color mediumGreyColor = Color(0xFFABABAB);
+  static const Color darkGreyColor = Color(0xFF666666);
+
+  /// Text Colors
+  static const Color textBlackColor = Color(0xFF000000);
+  static const Color textWhiteColor = Color(0xFFFFFFFF);
+
+  /// Light Colors
+  static const Color lightPrimaryColor = Color(0xFFFFE3B9);
+  static const Color lightGreyColor = Color(0xFFD9D9D9);
+  static const Color lightBlueColor = Color(0xFFAEC9FE);
+  static const Color lightPinkColor = Color(0xFFFE9E9E);
+  static const Color lightAccentColor = Color(0xFFFFECA5);
+
+}
